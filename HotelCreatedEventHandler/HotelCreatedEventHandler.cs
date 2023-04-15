6baf6c16e6384baa00d1293206bdcafabef61bdb
aussiearef <arefkr@gmail.com>
2023-04-15 19:11:18 +1000

    Added code

--- HotelCreatedEventHandler/HotelCreatedEventHandler.cs
+++ HotelCreatedEventHandler/HotelCreatedEventHandler.cs
@@ -1,11 +1,10 @@
 ﻿using System.Text.Json;
 using Amazon.DynamoDBv2;
-using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
 using Amazon.Lambda.SNSEvents;
 using HotelCreatedEventHandler.Models;
-using Nest;
 
 [assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
 
@@ -13,42 +12,19 @@ namespace HotelCreatedEventHandler;
 
 public class HotelCreatedEventHandler
 {
+    /*
+    *   Implement Idempotent Consumer pattern here.
+    */
+
     public async Task Handler(SNSEvent snsEvent)
     {
-        Console.WriteLine("Lambda was invoked.");
         var dbClient = new AmazonDynamoDBClient();
-        var table = Table.LoadTable(dbClient, "hotel-created-event-ids");
-
-        var host = Environment.GetEnvironmentVariable("host");
-        var userName = Environment.GetEnvironmentVariable("userName");
-        var password = Environment.GetEnvironmentVariable("password");
-        var indexName = Environment.GetEnvironmentVariable("indexName");
-
-        var connSettings = new ConnectionSettings(new Uri(host));
-        connSettings.BasicAuthentication(userName, password);
-        connSettings.DefaultIndex(indexName);
-        connSettings.DefaultMappingFor<Hotel>(m => m.IdProperty(p => p.Id));
-
-        var esClient = new ElasticClient(connSettings);
-
-        if (!(await esClient.Indices.ExistsAsync(indexName)).Exists) await esClient.Indices.CreateAsync(indexName);
-
-        Console.WriteLine($"Found {snsEvent.Records.Count} records in SNS Event");
+        using var dbContext = new DynamoDBContext(dbClient);
 
         foreach (var eventRecord in snsEvent.Records)
         {
-            var eventId = eventRecord.Sns.MessageId;
-            var foundItem = await table.GetItemAsync(eventId);
-            if (foundItem == null)
-                await table.PutItemAsync(new Document
-                {
-                    ["eventId"] = eventId
-                });
-
             var hotel = JsonSerializer.Deserialize<Hotel>(eventRecord.Sns.Message);
-
-            var response = await esClient.IndexDocumentAsync<Hotel>(hotel);
-            if (response.Result == Result.Error) Console.WriteLine($"Server Error:{response.ServerError.Error.Reason}");
+            await dbContext.SaveAsync(hotel);
         }
     }
 }
--- HotelCreatedEventHandler/Models/Hotel.cs
+++ HotelCreatedEventHandler/Models/Hotel.cs
@@ -1,13 +1,18 @@
+using Amazon.DynamoDBv2.DataModel;
+
 namespace HotelCreatedEventHandler.Models;
 
+[DynamoDBTable("Hotels_Order_Domain")]
 public class Hotel
 {
-    public string Name { get; set; }
-    public string CityName { get; set; }
+    [DynamoDBHashKey("userId")] public string? UserId { get; set; }
+
+    [DynamoDBRangeKey("Id")] public string? Id { get; set; }
+
+    public string? Name { get; set; }
+    public string? CityName { get; set; }
     public int Price { get; set; }
     public int Rating { get; set; }
-    public string UserId { get; set; }
-    public string Id { get; set; }
     public DateTime CreationDateTime { get; set; }
-    public string FileName { get; set; }
+    public string? FileName { get; set; }
 }
